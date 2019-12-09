bef950b02d0a2c4e267bf75d740ac09825f3a3cf
Kianoush97 <kianoush.abdal@gmail.com>
2019-12-09 11:34:34 +0100

    Added comments.

--- SEP1/src/MyDate.java
+++ SEP1/src/MyDate.java
@@ -1,3 +1,7 @@
+/**
+ * A class containing the date of exams.
+ * @version 1.0
+ */
 public class MyDate
 {
   private int day;
@@ -5,6 +9,13 @@ public class MyDate
   private int year;
   private String myDateTime;
 
+  /**
+   * A constructor initializing the Date.
+   * @param day to give it the day of the exam.
+   * @param month to give it the month of the exam.
+   * @param year to give it the year of the exam.
+   * @param myDateTime to give it time of the exam.
+   */
   public MyDate(int day, int month, int year, String myDateTime)
   {
     this.day = day;
@@ -13,25 +24,52 @@ public class MyDate
     this.myDateTime = myDateTime;
   }
 
+  /**
+   * To get the day of exam.
+   * @return int day.
+   */
   public int getDay()
   {
     return day;
   }
+
+  /**
+   * To get the month.
+   * @return int month.
+   */
 
   public int getMonth()
   {
     return month;
   }
 
+  /**
+   * To get the year.
+   * @return int year.
+   */
+
   public int getYear()
   {
     return year;
   }
 
+  /**
+   * To get the time of the exam.
+   * @return String time of the exam.
+   */
+
   public String getMyDateTime()
   {
     return myDateTime;
   }
+
+  /**
+   * A constructor to set the Date and time
+   * @param day set the day.
+   * @param month set the month.
+   * @param year set the year.
+   * @param myDateTime set the time.
+   */
 
   public void set(int day, int month, int year, String myDateTime)
   {
@@ -41,17 +79,30 @@ public class MyDate
     this.myDateTime = myDateTime;
   }
 
+  /**
+   * To make a copy of the date.
+   * @return a new date.
+   */
   public MyDate copy()
   {
     return new MyDate(day, month, year, myDateTime);
   }
 
+  /**
+   * To display the date and time of the exam.
+   * @return the day, month, year and time of the exam.
+   */
   public String toString()
   {
     return ("Day: " + day + ", Month: " + month + ", Year: " + year
         + ", My Date Time: " + myDateTime);
   }
 
+  /**
+   * To checks if two date objects are the same by comparing the fields.
+   * @param obj the date you want to be compared against.
+   * @return boolean.
+   */
   public boolean equals(Object obj)
   {
     if (!(obj instanceof MyDate))
