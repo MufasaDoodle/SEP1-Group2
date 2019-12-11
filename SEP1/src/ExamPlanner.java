76227e5583c0a553b54171f98548cad4a03d8e31
Kianoush97 <kianoush.abdal@gmail.com>
2019-12-11 12:42:48 +0100

    Exam planner

---
+++ SEP1/src/ExamPlanner.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class ExamPlanner extends Application
+{
+  public void start(Stage window) throws IOException
+  {
+    window.setTitle("Exam Planner");
+    FXMLLoader loader = new FXMLLoader();
+    loader.setLocation(getClass().getResource("ks.fxml"));
+    Scene scene = new Scene(loader.load());
+    window.setScene(scene);
+    window.show();
+  }
+}
+
